62d354877f4fb92a2f59f0b9b277b2750f40a131
Trawacho <sankturm@gmail.com>
2020-04-12 20:10:12 +0200

    removed unused usings

--- src/StockTV/Converter/GameModusToVisibilityConverter.cs
+++ src/StockTV/Converter/GameModusToVisibilityConverter.cs
@@ -1,9 +1,5 @@
 ﻿using StockTV.Classes;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 
