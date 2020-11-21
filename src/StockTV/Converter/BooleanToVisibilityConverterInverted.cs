736eb0fe2b5a5dc192e540c1cae302edcdb1e9d2
Trawacho <sankturm@gmail.com>
2020-11-21 21:05:46 +0100

    Zielbewerb, verbesserte Anzeige bei ungültigem Wert

---
+++ src/StockTV/Converter/BooleanToVisibilityConverterInverted.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace StockTV.Converter
+{
+    public class BooleanToVisibilityConverterInverted : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool b)
+            {
+                return b ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
