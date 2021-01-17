dd7234eaea90f900470aa24dfac53de9e0f421be
Trawacho <sankturm@gmail.com>
2021-01-17 09:48:29 +0100

    added MdnsService Class

---
+++ src/StockTV/Classes/MdnsService.cs
@@ -0,0 +1,32 @@
+﻿using Makaretu.Dns;
+using System;
+
+namespace StockTV.Classes
+{
+    public static class MdnsService
+    {
+        static ServiceProfile profile;
+        static ServiceDiscovery service;
+        internal static void Advertise()
+        {
+            if (profile == null)
+                profile = new ServiceProfile(Environment.MachineName, "_stockapp._tcp.local.", 4747);
+            if (service == null)
+                service = new ServiceDiscovery();
+
+            service.Advertise(profile);
+        }
+
+        internal static void Unadvertise()
+        {
+            service?.Unadvertise();
+            service?.Dispose();
+        }
+
+        public static void Dispose()
+        {
+            profile = null;
+            service?.Dispose();
+        }
+    }
+}
