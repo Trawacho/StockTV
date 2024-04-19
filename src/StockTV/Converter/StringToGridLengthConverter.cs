7f873c59b0f7be748bdf40fd427c9a0c11240dff
Trawacho <sankturm@gmail.com>
2024-04-19 17:40:56 +0200

    new StringToGridLengthConverter

---
+++ src/StockTV/Converter/StringToGridLengthConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace StockTV.Converter
+{
+    public class StringToGridLengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if(value is string v && !(string.IsNullOrEmpty(v)))
+            {
+                return new GridLength(1, GridUnitType.Star);
+            }
+            else
+            {
+                return new GridLength(0, GridUnitType.Pixel);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
