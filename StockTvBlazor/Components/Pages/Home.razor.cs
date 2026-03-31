146d36810db98a1dabee51848792284a0258f294
Alois Seidler <SeidlerAlois@juwin.bayern.de>
2026-03-31 09:16:43 +0200

    HomePage aktiviert mit Countdown

---
+++ StockTvBlazor/Components/Pages/Home.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorAppTests.Components.Pages
+{
+    public class HomeBase : ComponentBase, IDisposable
+    {
+        [Inject] protected NavigationManager NavManager { get; set; }
+
+        protected int countdown = 10;
+        protected int progress = 0;
+
+        private Timer timer;
+        private bool disposed = false;
+
+        protected override void OnInitialized()
+        {
+            int total = countdown;
+            timer = new Timer(async _ =>
+            {
+                if (countdown > 0)
+                {
+                    countdown--;
+                    progress = (int)((1 - (double)countdown / total) * 100);
+                    await InvokeAsync(StateHasChanged);
+                }
+                else
+                {
+                    timer.Dispose();
+                    NavManager.NavigateTo("/layouttest1");
+                }
+            }, null, 1000, 1000);
+        }
+
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                timer?.Dispose();
+                disposed = true;
+            }
+        }
+    }
+}
