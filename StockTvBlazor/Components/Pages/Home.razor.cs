de8dcf8e3d7fac212b2daa45b95ca73c08b03362
Alois Seidler <SeidlerAlois@juwin.bayern.de>
2026-04-09 14:40:54 +0200

    Home angepasst. Vorbereitet für 4 Cards. Diese dürfen noch designed werden

--- StockTvBlazor/Components/Pages/Home.razor.cs
+++ StockTvBlazor/Components/Pages/Home.razor.cs
@@ -5,80 +5,92 @@ namespace StockTvBlazor.Components.Pages;
 
 public class HomeBase : ComponentBase, IDisposable
 {
-	[Inject] protected NavigationManager? NavManager { get; set; }
-	[Inject] protected SettingsService? SettingsService { get; set; } // <-- Service injiziert
+    [Inject] protected NavigationManager? NavManager { get; set; }
+    [Inject] protected SettingsService? SettingsService { get; set; }
 
+    protected int countdown = 10;
+    protected int progress = 0;
 
-	protected int countdown = 10;
-	protected int progress = 0;
+    private Timer? _timer;
+    private Timer? _cardTimer;
+    private bool _disposed = false;
 
-	private Timer? _timer;
-	private bool _disposed = false;
+    protected int currentCardIndex = 0;
 
-	protected override void OnInitialized()
-	{
+    // Liste deiner Card-Komponenten
+    protected List<Type> CardComponents = new()
+    {
+        typeof(HomeCards.CardDisplay1),
+        typeof(HomeCards.CardDisplay2),
+        typeof(HomeCards.CardDisplay3),
+        typeof(HomeCards.CardDisplay4)
+    };
 
-	}
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            //if (System.Diagnostics.Debugger.IsAttached) { countdown = 3; }
 
-	protected override async Task OnAfterRenderAsync(bool firstRender)
-	{
-		if (firstRender)
-		{
-			if (System.Diagnostics.Debugger.IsAttached)
-			{
-				// Debug-Modus: Countdown auf 3 Sekunden setzen
-				countdown = 3;
-			}
+            StartCountdown();
+            StartCardRotation();
+        }
+    }
 
-			int total = countdown;
+    private void StartCountdown()
+    {
+        int total = countdown;
 
-			// Timer asynchron starten
-			_timer = new Timer(async _ =>
-			{
-				if (countdown > 0)
-				{
-					countdown--;
-					progress = (int)((1 - (double)countdown / total) * 100);
-					await InvokeAsync(StateHasChanged);
-				}
-				else
-				{
-					_timer?.Dispose();
-					NavigateToPage();
-				}
-			}, null, 1000, 1000);
-		}
-	}
+        _timer = new Timer(async _ =>
+        {
+            if (countdown > 0)
+            {
+                countdown--;
+                progress = (int)((1 - (double)countdown / total) * 100);
+                await InvokeAsync(StateHasChanged);
+            }
+            else
+            {
+                _timer?.Dispose();
+                NavigateToPage();
+            }
 
+        }, null, 1000, 1000);
+    }
 
-	public void Dispose()
-	{
-		if (!_disposed)
-		{
-			_timer?.Dispose();
-			_disposed = true;
-		}
-	}
+    private void StartCardRotation()
+    {
+        _cardTimer = new Timer(async _ =>
+        {
+            currentCardIndex++;
 
+            if (currentCardIndex >= CardComponents.Count)
+                currentCardIndex = 0; // Loop
 
-	private void NavigateToPage()
-	{
-		if(SettingsService == null || NavManager == null)
-		{
-			return;
-		}
+            await InvokeAsync(StateHasChanged);
 
-		var settings = SettingsService.CurrentSettings;
-		string _pageName = SettingsService.GetModusUrl(settings.Modus);
+        }, null, 2000, 2000);
+    }
 
+    private void NavigateToPage()
+    {
+        if (SettingsService == null || NavManager == null)
+            return;
 
-		//Hack: Für Tests hier die entsprechende Seite hart codieren, damit die Navigation funktioniert, ohne dass die SettingsService-Logik berücksichtigt werden muss.
-		//pageName = "layouttest";
+        var settings = SettingsService.CurrentSettings;
+        string pageName = SettingsService.GetModusUrl(settings.Modus);
 
-		if (!string.IsNullOrEmpty(_pageName))
-		{
-			NavManager.NavigateTo(_pageName);
-		}
+        if (!string.IsNullOrEmpty(pageName))
+            NavManager.NavigateTo(pageName);
+    }
 
-	}
+    public void Dispose()
+    {
+        if (_disposed) return;
+
+        _timer?.Dispose();
+        _cardTimer?.Dispose();
+
+        _disposed = true;
+    }
 }
